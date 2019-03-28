66e815586f9665947092e68b44198d4efce784c2
simjaemun2 <simjaemun2@naver.com>
2019-03-28 21:46:53 +0900

    update git ignore

--- howmuch_lpoint.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv("./point.csv", header=None)
-print(sum([int(''.join(filter(str.isdigit, s))) for s in df[2].tolist()]))
--- inumber.py
+++
@@ -1,7 +0,0 @@
-
-with open('/Users/a1/Desktop/point.csv', 'r') as file:
-    lines = file.readlines()
-    for s in filter(lambda x: x.startswith("* 쿠폰번호"), lines):
-        print(s.split()[3])
-
-
