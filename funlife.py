8f30c614d92cb77a6a1aa54777fe90ed545259b1
simjaemun2 <simjaemun2@naver.com>
2019-03-30 16:05:28 +0900

    added chrome option

--- funlife.py
+++ funlife.py
@@ -18,11 +18,12 @@ class Funlife:
 
     def __init_webdriver(self, config):
         options = webdriver.ChromeOptions()
+
+        options.add_argument("--disable-gpu")
         if config['webdriver']['headless'] == 'True':
             options.add_argument('headless')
-        options.add_argument('window-size=640x360')
-        options.add_argument("disable-gpu")
-        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument('no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         print(config['path']['webdriver'])
         driver = webdriver.Chrome(config['path']['webdriver'], options=options)
         driver.implicitly_wait(int(self.config['webdriver']['timeout']))
